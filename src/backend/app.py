726000fda81a57d25452f8970a09e73193ece242
Nguyen1611 <nguyen16112004@gmail.com>
2024-11-02 21:37:43 -0400

    add some comment to backend

--- src/backend/app.py
+++ src/backend/app.py
@@ -5,7 +5,7 @@ from auth import auth_bp  # Import the Blueprint
 app = Flask(__name__)
 CORS(app)  # Enable CORS for cross-origin requests
 
-# Register the auth Blueprint
+# Register the auth Blueprint that handle signin and register
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route('/')
